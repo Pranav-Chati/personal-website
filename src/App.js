1e090a7f9be25a98da7d739518247e087e052c9e
Pranav Chati <pchati.hack@gmail.com>
2023-06-30 09:52:37 -0400

    begin coding initial page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import {
   VStack,
   Code,
   Grid,
+  Image,
   theme,
+  HStack,
+  //extendTheme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -15,9 +18,71 @@ import { Logo } from './Logo';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+      <Box textAlign="center" fontSize="xl" >
+      <ColorModeSwitcher justifySelf="flex-end" />
+        <Box id="header">
+          <HStack>
+            <Link
+            colcolor="teal.500"
+            href=""
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              pranavchati
+            </Link>
+            <Link
+            colcolor="teal.500"
+            href="/about"
+            fontSize="2xl"
+            target="_blank"
+            rel="noreferrer"
+            >
+              about
+            </Link>
+            <Link
+            colcolor="teal.500"
+            href="/about"
+            fontSize="2xl"
+            target="_blank"
+            rel="noreferrer"
+            >
+              resume
+            </Link>
+            <Link
+              colcolor="teal.500"
+              href="/about"
+              fontSize="2xl"
+              target="_blank"
+              rel="noreferrer"
+              >
+                projects
+            </Link>
+            <Link
+              colcolor="teal.500"
+              href="/about"
+              fontSize="2xl"
+              target="_blank"
+              rel="noreferrer"
+              >
+                contact me
+            </Link>
+          </HStack>
+        </Box>
+        <VStack spacing={4} id="main-body">
+          <Image
+            borderRadius='full'
+            boxSize='150px'
+            src=''
+            alt='Pranav Chati'
+          />
+          <Text fontSize='5xl'> Pranav Chati </Text>
+          <Text fontSize={"medium"}>Student. Programmer. Entrepreneur.</Text>
+          {/* Icons Here */}
+        </VStack>
+
+        {/* <Grid minH="100vh" p={3}>
+          
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
@@ -33,7 +98,7 @@ function App() {
               Learn Chakra
             </Link>
           </VStack>
-        </Grid>
+        </Grid> */}
       </Box>
     </ChakraProvider>
   );
