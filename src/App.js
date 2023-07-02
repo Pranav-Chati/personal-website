ac4083723230932c321fa350a2409b72629b92d0
Pranav Chati <pchati.hack@gmail.com>
2023-07-01 22:04:25 -0400

    Cleaned up unneeded dependencies and install react-feather for icons

--- src/App.js
+++ src/App.js
@@ -7,98 +7,85 @@ import {
   VStack,
   Code,
   Grid,
+  Heading,
+  Container,
   Image,
-  theme,
   HStack,
-  //extendTheme,
+  extendTheme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { GitHub } from 'react-feather';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Comfortaa', sans-serif`,
+    body: `'Comfortaa', sans-serif`,
+  },
+})
+
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl" >
-      <ColorModeSwitcher justifySelf="flex-end" />
+        {/* All links will go here */}
         <Box id="header">
           <HStack>
             <Link
             colcolor="teal.500"
             href=""
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
+            fontSize="3xl"
             >
               pranavchati
             </Link>
             <Link
             colcolor="teal.500"
-            href="/about"
+            href="/resume"
             fontSize="2xl"
-            target="_blank"
-            rel="noreferrer"
-            >
-              about
-            </Link>
-            <Link
-            colcolor="teal.500"
-            href="/about"
-            fontSize="2xl"
-            target="_blank"
             rel="noreferrer"
             >
               resume
             </Link>
             <Link
               colcolor="teal.500"
-              href="/about"
+              href="/projects"
               fontSize="2xl"
-              target="_blank"
               rel="noreferrer"
               >
                 projects
             </Link>
             <Link
               colcolor="teal.500"
-              href="/about"
+              href="/contact_me"
               fontSize="2xl"
-              target="_blank"
               rel="noreferrer"
               >
                 contact me
             </Link>
           </HStack>
         </Box>
+
+        {/* Textual Information Here */}
         <VStack spacing={4} id="main-body">
           <Image
             borderRadius='full'
-            boxSize='150px'
-            src=''
+            boxSize='250px'
+            src='https://res.cloudinary.com/dvhcjpnix/image/upload/v1688141907/IMG_3214_kpk9az.jpg'
             alt='Pranav Chati'
           />
-          <Text fontSize='5xl'> Pranav Chati </Text>
-          <Text fontSize={"medium"}>Student. Programmer. Entrepreneur.</Text>
-          {/* Icons Here */}
-        </VStack>
-
-        {/* <Grid minH="100vh" p={3}>
+          <Heading fontSize='6xl'> Pranav Chati </Heading>
+          <Text fontSize={"3xl"}>Student. Programmer. Entrepreneur.</Text>
           
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid> */}
+
+          <Container fontSize='medium'> 
+            I'm a rising junior at Ohio State University studying Computer Science, Business, and Sustainability. At OSU, I'm apart of two main clubs: Student Consultants for Non-Profit Organizations (SCNO) and Ai Club. I am also apart of the Integrated Business and Engineering program. Check out my github or linkedin to learn more about me.
+          </Container>
+          <GitHub />
+
+
+        </VStack>
+        {/* Footer */}
+        <ColorModeSwitcher justifySelf="flex-end" />
       </Box>
     </ChakraProvider>
   );
